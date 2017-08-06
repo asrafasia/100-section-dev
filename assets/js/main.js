4ffa1e8b51769fa177ecd21411d09205feb8edf5
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-08-06 23:56:52 +0600

    class46

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,106 +1,116 @@
-(function ($) {
-	"use strict";
+(function($) {
+    "use strict";
 
-    jQuery(document).ready(function($){
+    jQuery(document).ready(function($) {
 
-$(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
         $(".carousel-inner .item:first-child").addClass("active");
-        
+
         $('[data-toggle="tooltip"]').tooltip();
 
         $('.staff-list').owlCarousel({
-            items : 4,
-            autoplay : false,
-            loop : true,
-            margin : 30,
-            nav : true,
-            navText : ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
-            
-            
-        });
-		
-		$('.testimonial-carousel').owlCarousel({
-            loop : true,
-            nav : false,
-            margin : 30,
-            items : 3,
-            dots : true,
-            autoplay : true
-            
-            
-        });
-		
-		
-		$('.homepage-slides').owlCarousel({
-            loop : true,
-            nav : false,
-            items : 1,
-            dots : true,
-            autoplay : true
-            
-            
+            items: 4,
+            autoplay: false,
+            loop: true,
+            margin: 30,
+            nav: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+
+
         });
 
-		 $('.product-list').masonry();
-		 
-		 
+        $('.testimonial-carousel').owlCarousel({
+            loop: true,
+            nav: false,
+            margin: 30,
+            items: 3,
+            dots: true,
+            autoplay: true
 
-         $(".search-btn").on('click', function(){
+
+        });
+
+
+        $('.homepage-slides').owlCarousel({
+            loop: true,
+            nav: false,
+            items: 1,
+            dots: true,
+            autoplay: true
+
+
+        });
+
+        $('.product-list').masonry();
+
+
+
+        $(".search-btn").on('click', function() {
             $(".search-box form").fadeToggle();
-             return false;
-        }); 
+            return false;
+        });
 
-		
-		jQuery(".project-hover a").magnificPopup({
-			type: 'image',
-			gallery: {
-				enabled: true,
+
+        jQuery(".project-hover a").magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true,
             },
-		});
-		
-		jQuery(window).load(function(){
-		  jQuery(".projects-list").isotope();
-		}); 
-		
-		jQuery(".projects-title li").on('click', function() {
+        });
+
+        jQuery(window).load(function() {
+            jQuery(".projects-list").isotope();
+        });
+
+        jQuery(".projects-title li").on('click', function() {
             $(".projects-title li").removeClass("active");
             $(this).addClass("active");
             var selector = $(this).attr('data-filter');
             $(".projects-list").isotope({
-               filter: selector 
+                filter: selector
             });
         });
-      
-		
 
-		var s = skrollr.init({
-			 render: function(data){
-				}
-		 });
+
+
+        var s = skrollr.init({
+            render: function(data) {}
+        });
 
 
         $(".video-btn").magnificPopup({
-			type: 'video',
-		});
+            type: 'video',
+        });
 
-		jQuery(window).scroll(function() {
-					if ($(this).scrollTop() > 100) {
-						$('.scrollToTop').fadeIn();
-					} else {
-						$('.scrollToTop').fadeOut();
-					}
-				});
+        jQuery(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scrollToTop').fadeIn();
+            } else {
+                $('.scrollToTop').fadeOut();
+            }
+        });
 
 
-				jQuery('.scrollToTop').click(function() {
-					$('html, body').animate({ scrollTop: 0 }, 800);
-					return false;
-				});
-		
+        jQuery('.scrollToTop').click(function() {
+            $('html, body').animate({ scrollTop: 0 }, 800);
+            return false;
+        });
+
+
+
+        jQuery('.featured-trailors-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            autoPlay: false,
+            nav: false,
+            dots: true,
+
+        });
+
 
     });
 
 
 
 
-}(jQuery));	
+}(jQuery));
