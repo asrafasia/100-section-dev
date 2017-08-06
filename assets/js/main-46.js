0b7d71417030eeb9955cbac5e067b8acf261563c
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-08-07 00:15:27 +0600

    46 two

---
+++ assets/js/main-46.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	jQuery('.featured-trailors-carousel').owlCarousel({
+    		items : 1,
+    		loop : true,
+    		autoPlay : false,
+    		nav : false,
+    		dots : true,
+
+    	});
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
