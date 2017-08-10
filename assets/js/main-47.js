5cf6c49e6a503ae076661281440b135328c5f295
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-08-10 18:07:20 +0600

    class47

---
+++ assets/js/main-47.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        jQuery(".testimonial-carousel").owlCarousel({
+            items : 1,
+            loop : true,
+            autoplay : true,
+            nav : false,
+            dots : true,
+        
+
+     });
+
+
+});
+
+    
+ 
+
+
+    jQuery(window).load(function(){
+
+        
+
+        
+    });
+
+
+}(jQuery));	
