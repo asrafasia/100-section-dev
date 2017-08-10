6bd906296cc549060c06c79c755720d5bc7a03df
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-08-10 22:05:58 +0600

    design-1 competetion

---
+++ design-1/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+      jQuery(document).ready(function(){
+
+			jQuery("#gallery").unitegallery();
+
+		});
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
