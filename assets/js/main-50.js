e8ab393b19dfde1cfc39217d1baa56b32e8c4d55
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-08-14 08:56:45 +0600

    class50

---
+++ assets/js/main-50.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       jQuery(".carousel-catagories").owlCarousel({
+       		items : 3,
+       		loop : true,
+       		dots : true,
+       		nav : false,
+       		margin :30,
+       		autoplay : true,
+       });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
