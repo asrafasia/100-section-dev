7a91b8116ff5dce553cf0bcbe1ff7aba887caadd
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-08-20 22:57:45 +0600

    class57

---
+++ assets/js/main-57.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	 $(".testimonial-author-names ul").perfectScrollbar();
+
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
