080bde25f8eed53f1065d214a53fc9354e1b46c2
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-08-23 12:31:19 +0600

    60 js 2

--- assets/js/main-60.js
+++ assets/js/main-60.js
@@ -130,7 +130,7 @@
                                 }]
             })
             .marker({
-                 position = Chittagong
+                 position : Chittagong
         		
             })
             .infowindow({
