147cd49d78c03160f17dda139dd565e855b5556e
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-08-26 06:10:14 +0600

    class62

---
+++ assets/js/main-62.js
@@ -0,0 +1,31 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	$('.project-filter li').on('click',function(){
+
+
+    		$('.project-filter li').removeClass('active');
+    		$(this).addClass('active');
+
+    		var selector = $(this).attr('data-filter');
+
+	    	$(".project-list").isotope({
+	    		filter : selector,
+	    	});
+
+    	});
+
+    	$(".project-list").isotope();
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
