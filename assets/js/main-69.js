4aafcd53518c80a0362b30125ee76046c07d16d3
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-09-05 17:18:34 +0600

    class 69 two

---
+++ assets/js/main-69.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       var footerHeight = $(".footer-area").height();
+        $(".site-main-wrapper").css('margin-bottom', footerHeight + 'px');
+        
+ 
+ 
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
