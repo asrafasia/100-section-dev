68b558824354a79d77f7204f72f58e8183985a64
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-09-06 15:25:59 +0600

    stickymenu

---
+++ stickymenu/assets/js/custom-66.js
@@ -0,0 +1,18 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    $(".logo-stick-area").sticky({topSpacing:0});	
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
