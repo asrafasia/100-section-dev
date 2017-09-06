4e64aafbda541b10482fdaa74c5a87aae563b344
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-09-06 07:08:21 +0600

    class71

---
+++ assets/js/main-71.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+         $('[data-toggle="popover"]').popover();
+
+         $('#example').popover(options);
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
