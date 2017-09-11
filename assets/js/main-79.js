ba3c919457aa43bd449165e7ad18b0a3e4ae251f
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-09-11 07:14:25 +0600

    class79

---
+++ assets/js/main-79.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $('.ajax-form').ajaxChimp({
+        	url: 'http://webforeveryone.us14.list-manage.com/subscribe/post?u=51527b67059b09289403abc09&amp;id=789a3be7b5'
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
