6ce0a8d2d7bb231906c3ed953bce77361364d56f
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-09-25 11:33:48 +0600

    class82

---
+++ assets/js/main-82.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	$('.feature-projects').owlCarousel({
+    		items : 1,
+    		loop : true,
+    		autoplay : true,
+    		dots : true,
+    		nav : false,
+
+    	});
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
