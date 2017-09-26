e3e788cb7cf057deb8b7d2c6c31cd0d0e2b15f2a
muhammad asraf uddin <asrafasiabd@gmail.com>
2017-09-26 17:35:56 +0600

    class85

---
+++ assets/js/main-85.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	$(".testimonial-carousel").owlCarousel({
+    		items : 1,
+    		nav : true,
+    		dots : false,
+    		navText : ["<i class='fa fa-long-arrow-left'>","<i class='fa fa-long-arrow-right'>"],
+    		loop : true,
+    		autoplay : false,
+    	});
+        
+
+
+
+        
+
+
+    });		//end document ready function
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
